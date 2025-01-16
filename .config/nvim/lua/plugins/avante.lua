25a070a9a0d03162039db92ec005e60a6998ca7d
Ricardo Kullock <ricardokullock@gmail.com>
2025-01-16 20:26:16 +0100

    small

--- .config/nvim/lua/plugins/avante.lua
+++ .config/nvim/lua/plugins/avante.lua
@@ -4,6 +4,14 @@ return {
   lazy = false,
   opts = {
     provider = "groq",
+    vendors = {
+      groq = {
+        __inherited_from = "openai",
+        api_key_name = "GROQ_API_KEY",
+        endpoint = "https://api.groq.com/openai/v1/",
+        model = "llama-3.3-70b-versatile",
+      },
+    }
   },
   build = "make BUILD_FROM_SOURCE=true",
   -- build = "make",
--- .config/nvim/lua/plugins/supermaven.lua
+++ .config/nvim/lua/plugins/supermaven.lua
@@ -1,23 +1,22 @@
 return {
   {
     "supermaven-inc/supermaven-nvim",
-    enable = false,
+    enable = true,
     event = 'InsertEnter',
-    opts = {
-      keymaps = {
-        accept_suggestion = nil, -- handled by nvim-cmp / blink.cmp
-      },
-      disable_inline_completion = true,
-      ignore_filetypes = { "bigfile", "snacks_input", "snacks_notif" },
-    },
-  }
-  -- lazy = false,
-  -- config = function()
-  --     require("supermaven-nvim").setup({
-  --       disable_inline_completion = true
-  --     })
-  --   end
-  -- },
+    -- opts = {
+    --   keymaps = {
+    --     accept_suggestion = nil, -- handled by nvim-cmp / blink.cmp
+    --   },
+    --   disable_inline_completion = true,
+    --   ignore_filetypes = { "bigfile", "snacks_input", "snacks_notif" },
+    -- },
+    lazy = false,
+    config = function()
+      require("supermaven-nvim").setup({
+        -- disable_inline_completion = true
+      })
+    end
+  },
 
   -- {
   --   "aliaksandr-trush/codeium.nvim", -- forked repo
