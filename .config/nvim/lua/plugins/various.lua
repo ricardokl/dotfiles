bdffabf2a78b14e99ae25265566e183119ccca1d
Ricardo Kullock <ricardokullock@gmail.com>
2025-04-09 16:28:39 +0200

    cleanup

--- .config/nvim/lua/plugins/various.lua
+++ .config/nvim/lua/plugins/various.lua
@@ -27,10 +27,6 @@ return {
     event = "VeryLazy",
   },
 
-  -- {
-  --   "jiangmiao/auto-pairs",
-  -- },
-
   {
     "numToStr/Navigator.nvim",
     config = function()
@@ -46,6 +42,28 @@ return {
   {
     "echasnovski/mini.statusline",
     version = false,
-    opts = {}
+    opts = {
+      content = {
+        active = function()
+          local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+          local git           = MiniStatusline.section_git({ trunc_width = 40 })
+          local diagnostics   = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+          local filename      = MiniStatusline.section_filename({ trunc_width = 900 })
+          local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 900 })
+          local location      = MiniStatusline.section_location({ trunc_width = 900 })
+          local search        = MiniStatusline.section_searchcount({ trunc_width = 75 })
+
+          return MiniStatusline.combine_groups({
+            { hl = mode_hl,                 strings = { mode } },
+            { hl = 'MiniStatuslineDevinfo', strings = { git, diagnostics } },
+            '%<', -- Mark general truncate point
+            { hl = 'MiniStatuslineFilename', strings = { filename } },
+            '%=', -- End left alignment
+            { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+            { hl = mode_hl,                  strings = { search, location } },
+          })
+        end
+      }
+    }
   },
 }
