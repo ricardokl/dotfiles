92ea851c1674a0a659be40eb985039b62c2d064c
Ricardo Kullock <ricardokullock@gmail.com>
2025-05-21 10:08:29 +0200

    lsp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,6 +4,7 @@ require("config.autocmds")
 require('config.keymaps')
 require('config.opts')
 require("config.lazy")
+-- require("config.lsp")
 vim.cmd.colorscheme("onedark")
 
 local has_aichat = pcall(require, "aichat_nvim")
@@ -26,36 +27,36 @@ vim.g.augment_workspace_folders = {
 }
 
 
-vim.lsp.config('*', {
-  capabilities = require('bink.cmp').get_lsp_capabilities()
-})
-
-vim.lsp.config('luals', {
-  settings = {
-    Lua = {}
-  },
-  on_init = function(client)
-    if client.workspace_folders then
-      local path = client.workspace_folders[1].name
-      if path ~= vim.fn.stdpath('config') and (vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc')) then
-        return
-      end
-    end
-    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-      runtime = {
-        version = 'LuaJIT'
-      },
-      workspace = {
-        checkThirdParty = false,
-        library = {
-          vim.env.VIMRUNTIME
-        }
-      }
-    })
-  end,
-})
-
-vim.lsp.enable('luals')
-vim.lsp.enable('pyright')
-vim.lsp.enable('vimls')
-vim.lsp.enable('rust_analyzer')
+-- vim.lsp.config('*', {
+--   capabilities = require('bink.cmp').get_lsp_capabilities()
+-- })
+--
+-- vim.lsp.config('luals', {
+--   settings = {
+--     Lua = {}
+--   },
+--   on_init = function(client)
+--     if client.workspace_folders then
+--       local path = client.workspace_folders[1].name
+--       if path ~= vim.fn.stdpath('config') and (vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc')) then
+--         return
+--       end
+--     end
+--     client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+--       runtime = {
+--         version = 'LuaJIT'
+--       },
+--       workspace = {
+--         checkThirdParty = false,
+--         library = {
+--           vim.env.VIMRUNTIME
+--         }
+--       }
+--     })
+--   end,
+-- })
+--
+-- vim.lsp.enable('luals')
+-- vim.lsp.enable('pyright')
+-- vim.lsp.enable('vimls')
+-- vim.lsp.enable('rust_analyzer')
