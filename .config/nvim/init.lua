cb6516a9d1e4ffcfc1ee8e9f700e466f41416bb6
Ricardo Kullock <ricardokullock@gmail.com>
2025-05-21 15:33:40 +0200

    Lsp with blink and configs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,10 +1,7 @@
-vim.g.augment_disable_tab_mapping = true
-
 require("config.autocmds")
 require('config.keymaps')
 require('config.opts')
 require("config.lazy")
--- require("config.lsp")
 vim.cmd.colorscheme("onedark")
 
 local has_aichat = pcall(require, "aichat_nvim")
@@ -17,46 +14,32 @@ if not has_augment then
   vim.notify("libaugment_extras lib not found", vim.log.levels.WARN)
 end
 
-vim.g.augment_workspace_folders = {
-  -- '~/projects/onedark_nvim_rs/',
-  -- '~/projects/aider/',
-  -- '~/projects/straico-client/',
-  -- '~/projects/straico-proxy/',
-  -- '~/projects/aichat_nvim/',
-  '~/projects/augment_extras/',
-}
+vim.lsp.config('luals', {
+  settings = {
+    Lua = {}
+  },
+  on_init = function(client)
+    if client.workspace_folders then
+      local path = client.workspace_folders[1].name
+      if path ~= vim.fn.stdpath('config') and (vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc')) then
+        return
+      end
+    end
+    client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+      runtime = {
+        version = 'LuaJIT'
+      },
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME
+        }
+      }
+    })
+  end,
+})
 
-
--- vim.lsp.config('*', {
---   capabilities = require('bink.cmp').get_lsp_capabilities()
--- })
---
--- vim.lsp.config('luals', {
---   settings = {
---     Lua = {}
---   },
---   on_init = function(client)
---     if client.workspace_folders then
---       local path = client.workspace_folders[1].name
---       if path ~= vim.fn.stdpath('config') and (vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc')) then
---         return
---       end
---     end
---     client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
---       runtime = {
---         version = 'LuaJIT'
---       },
---       workspace = {
---         checkThirdParty = false,
---         library = {
---           vim.env.VIMRUNTIME
---         }
---       }
---     })
---   end,
--- })
---
--- vim.lsp.enable('luals')
--- vim.lsp.enable('pyright')
--- vim.lsp.enable('vimls')
--- vim.lsp.enable('rust_analyzer')
+vim.lsp.enable('luals')
+vim.lsp.enable('pyright')
+vim.lsp.enable('vimls')
+vim.lsp.enable('rust_analyzer')
