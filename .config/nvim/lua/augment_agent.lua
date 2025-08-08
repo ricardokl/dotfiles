03b41dd48d6ef9515e870f7d0fc004e77390e691
Ricardo Kullock <ricardokullock@gmail.com>
2025-08-08 16:04:24 +0200

    Cleanup

--- .config/nvim/lua/augment_agent.lua
+++
@@ -1,9 +0,0 @@
-vim.api.nvim_create_autocmd("User", {
-  pattern = "ChatResponse",
-  callback = function()
-    vim.notify("Augment response recieved", vim.log.levels.INFO, {
-      title = "Augment Chat",
-      timeout = 5000 -- 5 seconds
-    })
-  end,
-})
